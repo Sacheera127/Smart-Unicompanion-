ff1c06d5ea0b7cc2ade146fe8a0ea989636135bb
madhawaaravinda05-dotco <madhawaaravinda05@gmail.com>
2026-06-23 18:28:34 +0530

    add postRepository for mangoDb cluster update

---
+++ smart-uni-backend/src/main/java/com/unicompanion/backend/repository/PostRepository.java
@@ -0,0 +1,17 @@
+package com.unicompanion.backend.repository;
+
+import com.unicompanion.backend.model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+    List<Post> findByUniversityAndStatus(String university, String status);
+
+    @org.springframework.data.mongodb.repository.Query()
+    List<Post> findByUniversityAndStatusAndCategory(String university, String status, String category);
+
+    List<Post> findByPostedById(String id);
+}
