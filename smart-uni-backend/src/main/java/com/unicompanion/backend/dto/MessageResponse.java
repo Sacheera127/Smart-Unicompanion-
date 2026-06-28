17b1a07fee574410c434b9eb6b27f7de92e4fa5f
Vikum Madhushanka <vikumworkmail@gmail.com>
2026-06-28 11:25:46 +0530

    Updated MessageResponse and removed some errors.

--- smart-uni-backend/src/main/java/com/unicompanion/backend/dto/MessageResponse.java
+++ smart-uni-backend/src/main/java/com/unicompanion/backend/dto/MessageResponse.java
@@ -3,8 +3,15 @@ package com.unicompanion.backend.dto;
 public class MessageResponse {
     private String message;
 
-    public MessageResponse(String message)
+    public MessageResponse(String message) {
+        this.message = message;
+    }
 
-    public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
